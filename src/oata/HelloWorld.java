d2545a99ec885ac1d7ee4b407b893ed806b2fb83
larry <liuxinjn@gmail.com>
2016-10-24 22:54:43 -0400

    test2

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -1,13 +1,9 @@
 package oata;
-
-import org.apache.log4j.Logger;
-import org.apache.log4j.BasicConfigurator;
-
 public class HelloWorld {
-    static Logger logger = Logger.getLogger(HelloWorld.class);
 
     public static void main(String[] args) {
-        BasicConfigurator.configure();
-        logger.info("Hello World");          // the old SysO-statement
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
     }
+
 }
